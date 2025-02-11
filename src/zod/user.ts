172f628308ca16e3be1ddf15a03835f35d035a7b
fados <fadeyworking@gmail.com>
2025-02-11 17:32:43 +0300

    1

--- src/zod/user.ts
+++ src/zod/user.ts
@@ -3,5 +3,5 @@ import { z } from 'zod';
 export const UserSchema = z.object({
     userLogin: z.string(),
     userPassword: z.string(),
-    userEmail: z.string(),
+    userEmail: z.string().email(),
 });
